cd40f639d69d23b11a8bb70ae4cf50d1574c89e1
ady243 <masiviady7@gmail.com>
2023-11-15 19:33:25 +0100

    comming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
 export default function Home() {
-
   return (
     <>
       <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
-      <h1>Commandez rapidement avec les références fast-food</h1>
-        <h1 className="text-5xl text-white font-bold mb-8 animate-pulse">
+        <h1 className="text-white -mt-12">Commandez rapidement avec les références fast-food</h1>
+        <h1 className="text-5xl text-white font-bold mb-8 animate-pulse mt-4">
           Bientôt
         </h1>
-        <p className="text-white text-xl mb-8">
-          "Nous mettons tout en œuvre pour vous offrir une expérience exceptionnelle. Restez connectés !"
+        <p className="text-white text-xl mb-8 mt-24">
+          &ldquo;Nous mettons tout en œuvre pour vous offrir une expérience exceptionnelle. Restez connectés !&rdquo;
         </p>
 
         <div>
-          <p>
-            &copy; 2023 VotreNom. Tous droits réservés.
+          <p className="text-white"> 
+            &copy; 2023 Ady masivi. Tous droits réservés.
           </p>
         </div>
       </div>
